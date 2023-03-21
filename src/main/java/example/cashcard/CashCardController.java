d002db6d9be7fb565c40fef2eb74748a99b85a60
khanhTran <khanhtrantung190894@gmail.com>
2023-03-22 00:09:25 +0700

    get http

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -5,12 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
     @GetMapping("/{requestId}")
-    public ResponseEntity<String> findByid() {
-        return ResponseEntity.ok("{}");
+    public ResponseEntity<CashCard> findByid(@PathVariable Long requestId) {
+        if (requestId.equals(99L)) {
+            CashCard cashCard = new CashCard(99L, 0.0);
+            return ResponseEntity.ok(cashCard);
+        }
+        return ResponseEntity.notFound().build();
 
     }
 }
